dba6663eb22900fc280cb891a1e80b84b00dde35
Daniel Engbert <end1@umbc.edu>
2023-01-05 08:26:41 -0500

    Create class Player, enum Action.

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,7 @@ FROM ros:noetic
 RUN apt-get update -y && apt-get install -y ros-noetic-compressed-image-transport
 RUN apt-get update -y && apt-get install -y python3-pip
 RUN pip install tensorflow keras torch
+RUN pip install pandas
 WORKDIR /root/projects/
 RUN echo 'catkin_make install && source /root/projects/devel/setup.bash' >> /root/.bashrc
+WORKDIR /root/projects/src
--- src/task1.py
+++ src/task1.py
@@ -1,37 +1,109 @@
 #!/usr/bin/env python3
-from __future__ import print_function
 import argparse
+import cv2
+from enum import Enum
 
 import time
 import numpy as np
 import pandas as pd
 
+import pdb
+import prey
 import robobo
-import cv2
+from robobo.base import Robobo
 import sys
 import signal
-import prey
-from enum import Enum
+from typing import Union
+
 
 class Action(Enum):
-    FORWARD = "forward"
-    BACKWARD = "forward"
+    FORWARD = "f"
+    BACKWARD = "b"
+    ROTATE_L = "rot_L"
+    ROTATE_R = "rot_R"
+
+
+class Player:
+    IP: str
+    use_sim: bool
+    rob: Robobo
+
+    def __init__(self, IP="127.0.0.1", use_sim: bool = True):
+        self.IP = IP
+        self.use_sim = use_sim
+        # TODO: add headless arg for sim?
+        if use_sim:
+            self.rob = robobo.SimulationRobobo().connect(
+                address="127.0.0.1", port=19997
+            )
+        else:
+            # self.rob = robobo.HardwareRobobo(camera=True).connect(address="192.168.1.7")
+            self.rob = robobo.HardwareRobobo().connect(address=IP)
+
+    def play(self):
+        if self.use_sim:
+            self.rob.play_simulation()
+
+        self.apply_action(Action.FORWARD)
+        pdb.set_trace()
+        s = self.get_state()
+
+        # when done
+        if self.use_sim:
+            # pause the simulation and read the collected food
+            self.rob.pause_simulation()
+
+            # Stopping the simualtion resets the environment
+            self.rob.stop_world()
+
+    def get_state(self, with_img=True):
+        raw_irs = self.rob.read_irs()
+        irs = np.log(np.array(raw_irs)) / 10
+
+        img = None
+        if with_img:
+            img = self.rob.get_image_front()
+        return {"irs": irs, "img": img}
+
+    def apply_action(self, a: Union[Action, str], dur: float = 2000, power: float = 25):
+        """
+        params:
+            dur: duration (ms) of action
+            power: float in range [0, 100]
+        """
+        a = Action(a)
+        if a == Action.FORWARD:
+            self.rob.move(power, power, dur)
+        elif a == Action.BACKWARD:
+            self.rob.move(-power, -power, dur)
+        elif a == Action.ROTATE_L:
+            self.rob.move(-power, power, dur)
+        elif a == Action.ROTATE_R:
+            self.rob.move(power, -power, dur)
+        else:
+            raise NotImplementedError(f"unhandled action {a}")
 
 
 def terminate_program(signal_number, frame):
     print("Ctrl-C received, terminating program")
     sys.exit(1)
 
+
 def main():
+
     # TODO: add headless arg for sim?
 
     use_sim = True
-    IP ='10.15.2.126' # hardware IP
+    IP = "10.15.2.126"  # hardware IP
 
     signal.signal(signal.SIGINT, terminate_program)
 
+    player = Player(use_sim=use_sim, IP=IP)
+    player.play()
+    exit(0)
+
     if use_sim:
-        rob = robobo.SimulationRobobo().connect(address='127.0.0.1', port=19997)
+        rob = robobo.SimulationRobobo().connect(address="127.0.0.1", port=19997)
         rob.play_simulation()
     else:
         # rob = robobo.HardwareRobobo(camera=True).connect(address="192.168.1.7")
@@ -60,21 +132,20 @@ def main():
     #  can you read global position in sim?  (we could reward "exploration" of env grid)
     #  how to detect collsions?
 
-    # part 2: 
+    # part 2:
     # save (downsampled) samera images, run green mask over them, filter out small blobs
 
     # train a simple DQN using IR states and sampled
 
-
     # Following code moves the robot
     for i in range(50):
-        #print("robobo is at {}".format(rob.position()))
+        # print("robobo is at {}".format(rob.position()))
         rob.move(5, 5, 2000)
-        irs = np.log(np.array(rob.read_irs()))/10
+        irs = np.log(np.array(rob.read_irs())) / 10
         print("ROB Irs: {}".format(irs))
-        #print("Base sensor detection: ", rob.base_detects_food())
-   
-    #print("robobo is at {}".format(rob.position()))
+        # print("Base sensor detection: ", rob.base_detects_food())
+
+    # print("robobo is at {}".format(rob.position()))
     # Following code gets an image from the camera
 
     image = rob.get_image_front()
@@ -86,7 +157,7 @@ def main():
     if use_sim:
         # pause the simulation and read the collected food
         rob.pause_simulation()
-        
+
         # Stopping the simualtion resets the environment
         rob.stop_world()
 
